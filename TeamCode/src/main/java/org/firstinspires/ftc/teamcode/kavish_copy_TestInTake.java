631c6cc9d45ea4ed2e18547d34effff716bfa30f
k3mah <mahasethkavish@gmail.com>
2025-10-05 17:36:23 -0400

    kavisg did something

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kavish_copy_TestInTake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kavish_copy_TestInTake.java
@@ -63,7 +63,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 
 @TeleOp(name="KavishVersionIntake", group="Linear OpMode")
-
 public class kavish_copy_TestInTake extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
