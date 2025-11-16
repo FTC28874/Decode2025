eb31af2b01580291d63ff7f45c27a1eac6833862
hanumankind1 <johanreads@gmail.com>
2025-11-16 14:45:12 -0500

    67

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/apriltagtrackingthingy.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.auto;
+import org.firstinspires.ftc.teamcode.mechanisms.apriltag;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous
+public class apriltagtrackingthingy extends OpMode{
+    apriltag apriltag = new apriltag();
+
+    @Override
+    public void init() {
+        apriltag.init(hardwareMap, telemetry);
+    }
+
+    @Override
+    public void loop() {
+        apriltag.update();
+        AprilTagDetection id20 = apriltag.getTagBySpecificId(20);
+        telemetry.addData("id20 string", id20.toString());
+    }
+}
