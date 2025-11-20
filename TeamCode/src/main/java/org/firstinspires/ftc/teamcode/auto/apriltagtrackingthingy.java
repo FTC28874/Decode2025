5073f356919512b7193c0af61ffbb444590799c7
hanumankind1 <johanreads@gmail.com>
2025-11-20 18:15:37 -0500

    ffhrvgfuhglbufe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/apriltagtrackingthingy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/apriltagtrackingthingy.java
@@ -1,23 +1,22 @@
 package org.firstinspires.ftc.teamcode.auto;
-import org.firstinspires.ftc.teamcode.mechanisms.apriltag;
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.teamcode.mechanisms.AprilTagWebcam;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 @Autonomous
-public class apriltagtrackingthingy extends OpMode{
-    apriltag apriltag = new apriltag();
-
+public class apriltagtrackingthingy extends OpMode {
+    AprilTagWebcam aprilTagWebcam = new AprilTagWebcam();
     @Override
     public void init() {
-        apriltag.init(hardwareMap, telemetry);
+        aprilTagWebcam.init(hardwareMap, telemetry);
     }
 
     @Override
     public void loop() {
-        apriltag.update();
-        AprilTagDetection id20 = apriltag.getTagBySpecificId(24);
-        telemetry.addData("id20 string", id20.toString());
+        aprilTagWebcam.update();
+        AprilTagDetection id24 = aprilTagWebcam.getTagBySpecificId(24);
+        aprilTagWebcam.displayDetectionTelemetry(id24);
+        telemetry.addData("id24 string", id24.toString());
     }
 }
