cbe47ed64386f073109d528db2f6436707ff6e2f
hanumankind1 <johanreads@gmail.com>
2025-11-23 16:50:37 -0500

    added PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PIDcontrol.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PIDcontrol.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 @TeleOp
 public class PIDcontrol extends LinearOpMode {
     DcMotorEx motor;
